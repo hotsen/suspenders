e944cca429bcbbafb37d9033a0be8055ae143440
Alexander Hoth <alexander.hoth@me.com>
2017-01-19 00:38:43 +0100

    Freezing ruby version to 2.4.0

--- lib/suspenders/version.rb
+++ lib/suspenders/version.rb
@@ -1,8 +1,5 @@
 module Suspenders
-  RAILS_VERSION = "~> 5.0.1".freeze
-  RUBY_VERSION = IO.
-    read("#{File.dirname(__FILE__)}/../../.ruby-version").
-    strip.
-    freeze
+  RAILS_VERSION = "5.0.1".freeze
+  RUBY_VERSION = "2.4.0".freeze
   VERSION = "1.43.0".freeze
 end
